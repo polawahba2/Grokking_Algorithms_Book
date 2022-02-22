47388b36e2d783c1d29caee556127438e86f46fc
pola <pola.wahba2@gmail.com>
2022-02-23 00:11:06 +0200

    improve the Big O time

--- Recursion/QuickSort.dart
+++ Recursion/QuickSort.dart
@@ -1,5 +1,5 @@
 void main() {
-  List l = [3, 5, 2, 1, 4, 6,22,12,2,4,1];
+  List l = [3, 5, 2, 1, 4, 6, 22, 12, 2, 4, 1];
   print(quickSort(l));
 }
 
@@ -7,7 +7,8 @@ List quickSort(List arr) {
   if (arr.length < 2)
     return arr; // this mean that the array empty or have only one element
   List lessElements = [], greaterElements = [];
-  int pivot = arr[0];
+  int pivot = arr[arr.length ~/
+      2]; //select the middle element in the array to get the best case for the algoritm
   arr.remove(pivot);
   arr.forEach((element) {
     if (element <= pivot) {
